9f1a128173eab927e3798e400c5a02031afa4cb7
Julian <mail@julianrijken.com>
2024-03-01 09:06:03 +0100

    Trying to add glm

--- CommandLineProject/CommandLine/CommandLine.cpp
+++ CommandLineProject/CommandLine/CommandLine.cpp
@@ -1,19 +1,68 @@
 ﻿#include "CommandLine.h"
 #include <nlohmann/json.hpp>
+//#include <glm/vec3.hpp>
 
 #include <filesystem>
+#include <fstream>
+
 
 using namespace std;
 
+//struct Block
+//{
+//	std::string layer;
+//	bool opaque;
+//	std::vector<glm::vec3> positions;
+//};
+
 int main()
 {
+	const std::filesystem::path filePath = "./resources/minecraftscene.json";
 
-	std::filesystem::path filePath = "./resources/minecraftscene.json";
+	if (not exists(filePath))
+		throw std::runtime_error("File does not exist: " + filePath.string());
 
-	
+	std::ifstream inFile;
+	inFile.open(filePath);
+
+	if (not inFile.is_open())
+		throw std::runtime_error("File already open" + filePath.string());
+
+	std::cout << "File Opened: " << filePath << '\n' << '\n';
+	std::cout << "Json Input: " << inFile.get() << '\n' << '\n';
+
+	try
+	{
+		nlohmann::json doc;
+		inFile >> doc;
+
+		std::cout << "Parsing successful" << '\n';
+
+		//std::unordered_set<Block> blocks{};
+
+		for (const auto& obj : doc)
+		{
+			// Extract the "type" and "loc" values
+			std::string type = obj["type"];
+			std::vector<int> loc = obj["loc"].get<std::vector<int>>();
 
 
-
+			// Print the extracted values
+			std::cout << "Type: " << type << ", Location: " << "[";
+			for (size_t i = 0; i < loc.size(); ++i)
+			{
+				std::cout << loc[i];
+				if (i < loc.size() - 1)
+					std::cout << ", ";
+			}
+			std::cout << "]" << '\n';
+		}
+	}
+	catch (nlohmann::json::parse_error& e)
+	{
+		std::cerr << "Error parsing JSON: " << e.what() << '\n';
+		return -1;
+	}
 
 
 #ifdef EXAMPLE1
